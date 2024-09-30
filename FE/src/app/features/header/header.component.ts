b4edfd3892419ba669c652a98aa6334d0e6b846d
caosyhoang <mhoangkt8@gmail.com>
2024-10-01 00:07:47 +0700

    30/09

--- FE/src/app/features/header/header.component.ts
+++ FE/src/app/features/header/header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, NgbNavModule],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- FE/src/app/features/home/home.component.ts
+++ FE/src/app/features/home/home.component.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-import { FooterComponent } from '../footer/footer.component';
-import { ListPostComponent } from '../list-post/list-post.component';
-import { AutoCompleteComponent } from '../../shared/components/app_inputs/auto-complete/auto-complete.component';
-import { ListFeedbackComponent } from '../list-feedback/list-feedback.component';
+import { SliderComponent } from '../slider/slider.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    NgbNavModule,
-    AutoCompleteComponent,
-    FooterComponent,
-    ListFeedbackComponent,
-    ListPostComponent,
-  ],
+  imports: [HeaderComponent, SliderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
